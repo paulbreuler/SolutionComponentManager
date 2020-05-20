6680f267bda6bcdc5af8d3d887a09bfc79ebf71c
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2020-05-20 16:33:09 -0400

    Added additional default field for SolutionComponent

--- src/SolutionManagement/SolutionComponent.ts
+++ src/SolutionManagement/SolutionComponent.ts
@@ -1,14 +1,24 @@
 import { ComponentTypes } from "../componentTypes"
 
 export class SolutionComponent {
+    // Default 
     public objectId: string;
     public friendlyName?: string;
     public logicalName: string;
     public componentType: number;
-    public componentTypeName?: string;
     public solutionId: string;
+    public createdon: Date;
+    public rootcomponentbehavior: number;
+    public modifiedon: Date;
+    public versionnumber: number;
+    public ismetadata: boolean;
     public solutionnName: string;
+    
+    // Extended functionality 
+    public componentTypeName?: string;
+
     public children?: SolutionComponent[];
+
     instantiateFromJson(element: any) {
         this.objectId = element.objectid;
         this.componentType = element.componenttype;
